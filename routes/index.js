6a47848aeaee8e935034bf77daf2d37398b9da1f
Rifki Rahadian <rrahadian94@gmail.com>
2020-06-19 17:03:18 +0700

    Middleware: Auth

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,14 @@ const validator = require('../modules/validator')
 
 module.exports = (app, express) => {
   const apiRoutes = express.Router()
+  const authRoutes = express.Router()
+
+  //middleware
+  require('../middlewares/Auth')(authRoutes)
 
   apiRoutes.post('/register',validator.register, AuthController.register)
   apiRoutes.post('/login', validator.login, AuthController.login)
 
   app.use('/api', apiRoutes)
+  app.use('/api', authRoutes)
 }
