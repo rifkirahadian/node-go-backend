1298aabf8cc91891f57e4b7d4ddd0c90a85dd56d
Rifki Rahadian <rrahadian94@gmail.com>
2020-06-20 13:42:24 +0700

    Config: Custom Validator

--- go/main.go
+++ go/main.go
@@ -1,8 +1,17 @@
 package main
 
 import (
-	"github.com/labstack/echo"
+  "gopkg.in/go-playground/validator.v9"
+  "github.com/labstack/echo"
 )
+
+type CustomValidator struct {
+	validator *validator.Validate
+}
+
+func (cv *CustomValidator) Validate(i interface{}) error {
+	return cv.validator.Struct(i)
+}
 
 func main()  {
 	e := echo.New()
